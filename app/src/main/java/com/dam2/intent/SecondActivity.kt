d7821cccb9d30b1e3cbfee099a06a69cf07f7da6
EricBorder <magicsergy@gmail.com>
2023-01-16 09:55:21 +0100

    Añado nueva variable

--- app/src/main/java/com/dam2/intent/SecondActivity.kt
+++ app/src/main/java/com/dam2/intent/SecondActivity.kt
@@ -21,22 +21,27 @@ class SecondActivity : AppCompatActivity() {
         textView.text = message.toString()
 
 
-        val botonsalir = findViewById<Button>(R.id.salir)
+        val botonsalir = findViewById<Button>(R.id.suma)
         botonsalir.setOnClickListener {
             //val intento2 = Intent(this, SecondActivity::class.java)
-            intent.putExtra("saludo", "Hola")
-            intent.putExtra("despedida", "adios")
-            intent.putExtra("despedida2", "chao")
+            val numero1 = intent.getIntExtra("EXTRA_MESSAGE", 7)
+            val numero2 = intent.getIntExtra("NUMERO2", 7)
+            intent.putExtra("Resultado", numero1 + numero2)
+            //intent.putExtra("saludo", "Hola")
+            //intent.putExtra("despedida", "Adios")
+            //intent.putExtra("despedida2", "Chao")
             Log.d("mensaje", "actualizado")
             /* startActivity(intento2)*/
             setResult(Activity.RESULT_OK, intent)
             finish()
         }
-        val botonsalir1 = findViewById<Button>(R.id.salir2)
+        val botonsalir1 = findViewById<Button>(R.id.resta)
         botonsalir1.setOnClickListener {
             //val intento2 = Intent(this, SecondActivity::class.java)
             // Meto un nuevo dato en el intent
-            intent.putExtra("despedida", "adios")
+            val numero1 = intent.getIntExtra("EXTRA_MESSAGE", 7)
+            val numero2 = intent.getIntExtra("NUMERO2", 7)
+            intent.putExtra("Resultado2", numero1 - numero2)
             Log.d("mensaje", "actualizado")
             /* startActivity(intento2)*/
             // Configuro el result para que la Main lo tenga disponible
@@ -47,10 +52,12 @@ class SecondActivity : AppCompatActivity() {
             // cierro la activity
             finish()
         }
-        val botonsalir2 = findViewById<Button>(R.id.salir3)
+        val botonsalir2 = findViewById<Button>(R.id.multiplicar)
         botonsalir2.setOnClickListener {
             //val intento2 = Intent(this, SecondActivity::class.java)
-            intent.putExtra("despedida2", "chao")
+            val numero1 = intent.getIntExtra("EXTRA_MESSAGE", 7)
+            val numero2 = intent.getIntExtra("NUMERO2", 7)
+            intent.putExtra("Resultado3", numero1 * numero2)
             Log.d("mensaje", "actualizado")
             /* startActivity(intento2)*/
             setResult(Activity.RESULT_OK, intent)
